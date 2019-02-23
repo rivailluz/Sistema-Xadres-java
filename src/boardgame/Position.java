3a26073985e64080cce99f50c70910bc2c79693b
rivailluz <rivailluz@gmail.com>
2019-02-23 19:38:44 -0400

    Inclusão do Metodo position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,23 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+   
+    
+    public String toString{
+        return row + ', ' +     }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
