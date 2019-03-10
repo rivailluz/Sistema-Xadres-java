e8c946849c922093d922a3eea63deb2910312ba5
rivailluz <rivailluz@gmail.com>
2019-03-10 00:13:35 -0400

    Handling exceptions and clearing screen

--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -28,6 +28,12 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console 
+    public static void clearScreen() {       
+        System.out.print("\033[H\033[2J");       
+        System.out.flush();   
+    }   
+    
     public static void printBoard(ChessPiece piece[][]) {
         for (int i = 0; i < piece.length; i++) {
             System.out.print((8 - i) + " ");
@@ -42,12 +48,12 @@ public class UI {
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
             System.out.print("-");
-        } else {
-            if (piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            } else {
-                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
-            }
+        } 
+        else if (piece.getColor() == Color.WHITE) {
+            System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+        } 
+        else {
+            System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
         }
         System.out.print(" ");
     }
@@ -60,7 +66,7 @@ public class UI {
             return new ChessPosition(column, row);
         }
         catch(RuntimeException e){
-            throw new InputMismatchException("Erro lendo posição do xadrez. Valores validos de A1 a H8");
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
         }
     }
 }
